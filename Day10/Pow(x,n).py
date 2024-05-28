8d917194d566899cea10c1f8cde2417277c6f68b
LawrenceLi <lawrencelzl1922@gmail.com>
2024-05-28 15:33:57 +0800

    update day10 pow

--- Day10/Pow(x,n).py
+++ Day10/Pow(x,n).py
@@ -1,14 +1,20 @@
-def myPow(x,n):
+def myPow(x, n):
     if x == 0.0:
         return 0.0
-    
+
     res = 1
-    
+
     while n:
+        # n & 1等价于取n % 2
         if n & 1:
             res *= x
-        
+
+        # x直接变为x^2
         x *= x
+        # n // 2
         n >>= 1
-        
+
     return res
+
+
+# 时间复杂度为O(lgn)
